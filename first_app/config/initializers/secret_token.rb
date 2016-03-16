28d0e37f0e177bfc686f663a19d561f483666953
Tetsuji-Kawatsuji-FXNN <tetsuji.kawatsuji@fxnn.fujixerox.co.jp>
2016-03-16 10:38:54 +0900

    Initialize ropository

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '90aee8b61ff8d5ce1bcfefb0835c012dfdb98ec6c32f6d3e5b77d3f7a0b53aa3059888f13719b605cdfd8c0f812d73baf069a96df09fcf7583b789f51a1b00b6'
